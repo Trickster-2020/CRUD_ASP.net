83fcd4d5107b8608380d8a52cce979186c7d4a4c
Trickster-2020 <karna.sujan52@gmail.com>
2023-06-22 15:01:50 +0545

    Added crud with validation

---
+++ WebSite35/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    SqlConnection conn = new SqlConnection("Data Source=saitama;Initial Catalog=crud;Integrated Security=True");
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        TextBox name = GridView1.FooterRow.FindControl("name") as TextBox;
+        TextBox user=GridView1.FooterRow.FindControl("user") as TextBox;
+        TextBox pass=GridView1.FooterRow.FindControl("pass") as TextBox;
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("INSERT into details values('" + name.Text + "','" + user.Text + "','" + pass.Text + "')",conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        GridView1.DataBind();
+    }
+
+  
+}
